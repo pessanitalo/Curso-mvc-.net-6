f538b9121575a1971e8b911260a7cae456282871
pessanitalo <italopessan@gmail.com>
2022-05-24 21:03:31 +0044

    Ifornecedor service

---
+++ DevIo.Business/Interfaces/IFornecedorService.cs
@@ -0,0 +1,14 @@
+﻿using DevIO.Bussiness.Models;
+
+namespace DevIO.Business.Interfaces
+{
+    public interface IFornecedorService : IDisposable
+    {
+        Task Adicionar(Fornecedor fornecedor);
+        Task Atualizar(Fornecedor fornecedor);
+        Task Remover(Guid id);
+
+        //fornecedor possui endereço
+        Task AtualizarEndereco(Endereco endereco);
+    }
+}
